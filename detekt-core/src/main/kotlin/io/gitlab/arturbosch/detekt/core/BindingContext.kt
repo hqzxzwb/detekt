6e1b5e27ec45be04203ea31d8081934276169e7c
Artur Bosch <arturbosch@gmx.de>
2020-05-27 19:13:15 +0200

    Extract binding context creation

---
+++ detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/BindingContext.kt
@@ -0,0 +1,48 @@
+package io.gitlab.arturbosch.detekt.core
+
+import org.jetbrains.kotlin.cli.common.messages.AnalyzerWithCompilerReport
+import org.jetbrains.kotlin.cli.common.messages.CompilerMessageLocation
+import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity
+import org.jetbrains.kotlin.cli.common.messages.PlainTextMessageRenderer
+import org.jetbrains.kotlin.cli.common.messages.PrintingMessageCollector
+import org.jetbrains.kotlin.cli.jvm.compiler.NoScopeRecordCliBindingTrace
+import org.jetbrains.kotlin.cli.jvm.compiler.TopDownAnalyzerFacadeForJVM
+import org.jetbrains.kotlin.config.languageVersionSettings
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.resolve.BindingContext
+import org.jetbrains.kotlin.resolve.lazy.declarations.FileBasedDeclarationProviderFactory
+
+internal fun generateBindingContext(settings: ProcessingSettings, files: List<KtFile>): BindingContext {
+    val classpath = settings.classpath
+    val environment = settings.environment
+
+    return if (classpath.isNotEmpty()) {
+        val analyzer = AnalyzerWithCompilerReport(
+            PrintingMessageCollector(System.err, DetektMessageRenderer, true),
+            environment.configuration.languageVersionSettings
+        )
+        analyzer.analyzeAndReport(files) {
+            TopDownAnalyzerFacadeForJVM.analyzeFilesWithJavaIntegration(
+                environment.project, files, NoScopeRecordCliBindingTrace(),
+                environment.configuration, environment::createPackagePartProvider,
+                ::FileBasedDeclarationProviderFactory
+            )
+        }
+        analyzer.analysisResult.bindingContext
+    } else {
+        BindingContext.EMPTY
+    }
+}
+
+private object DetektMessageRenderer : PlainTextMessageRenderer() {
+    override fun getName() = "detekt message renderer"
+    override fun getPath(location: CompilerMessageLocation) = location.path
+    override fun render(
+        severity: CompilerMessageSeverity,
+        message: String,
+        location: CompilerMessageLocation?
+    ): String {
+        if (!severity.isError) return ""
+        return super.render(severity, message, location)
+    }
+}
